92b01df3d9b331d018ecb789395b07d6a7117937
Brittany Garcia <britt.garcia123@gmail.com>
2018-04-09 14:26:55 -0700

    added type writer font style to my name

---
+++ main.js
@@ -0,0 +1,20 @@
+var i = 0;
+var text = "Brittany Garcia";
+var $name = document.querySelector("#name");
+var speedRange = [200, 300];
+
+writeLetter();
+
+function getSpeed() {
+    return speedRange[0] + (Math.random() * (speedRange[1] - speedRange[0]));
+}
+
+function writeLetter(){
+    $name.innerHTML += text[i];
+    i++;
+
+    if(i < text.length){
+        setTimeout(writeLetter, getSpeed());
+
+    }
+}
